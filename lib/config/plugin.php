3aaf4c8e774532b6a8e470ef5f1e931216fc8f98
kl <kl@10.66.3.31>
2014-05-07 13:14:11 +0600

    создание репозитория

---
+++ lib/config/plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'name' => 'Товарный чек',
+    'description' => 'Для использования на территории РФ.',
+    'img' => 'img/cashmemo.png',
+    'vendor' => '985310',
+    'version' => '1.0.0',
+    'printform' => true,
+    'handlers' => array(),
+);
+
+//EOF
---
+++ lib/config/settings.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    'COMPANYNAME' => array(
+        'value' => '',
+        'title' => 'Название компании',
+        'description' => 'Укажите название организации, от имени которой выписывается счет',
+        'control_type' => 'text',
+        'subject' => 'printform',
+    ),
+    'COMPANYADDRESS' => array(
+        'value' => '',
+        'title' => 'Адрес компании',
+        'description' => 'Укажите адрес организации, от имени которой выписывается счет',
+        'control_type' => 'textarea',
+        'subject' => 'printform',
+    ),
+    'OGRN' => array(
+        'value' => '',
+        'title' => 'ОГРН',
+        'description' => 'ОГРН организации',
+        'control_type' => 'text',
+        'subject' => 'printform',
+    ),
+);
